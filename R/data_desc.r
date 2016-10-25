752e8d6296a96264a9ebff2c634c6c407f48d07d
Mike McMahon <mike.mcmahon@dfo-mpo.gc.ca>
2016-10-25 16:17:20 -0300

    modifies isobath docs

--- R/data_desc.r
+++ R/data_desc.r
@@ -1,4 +1,5 @@
 #' @name isobaths
+#' @title isobaths
 #' @description  Includes isobaths for the following depths (m):
 #'  "10"   "20"   "50"   "75"   "100"  "200"  "250"  "300"  "400"  "500"  "600"  "700"  "750"  "800"
 #'  "900"  "1000" "1200" "1250" "1400" "1500" "1750" "2000" "2500" "3000" "4000" "5000"
