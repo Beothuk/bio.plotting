befcf95de630474f5216947d16ddfffaaaf27819
Mike McMahon <mike.mcmahon@dfo-mpo.gc.ca>
2016-10-25 16:15:00 -0300

    modifies isobath docs

--- R/data_desc.r
+++ R/data_desc.r
@@ -1,8 +1,7 @@
-#' This is the contents of the isobaths
-#' Includes isobaths for the following depths (m):
+#' @name isobaths
+#' @description  Includes isobaths for the following depths (m):
 #'  "10"   "20"   "50"   "75"   "100"  "200"  "250"  "300"  "400"  "500"  "600"  "700"  "750"  "800"
 #'  "900"  "1000" "1200" "1250" "1400" "1500" "1750" "2000" "2500" "3000" "4000" "5000"
-#' @name isobaths
 #' @docType data
 #' @author  Jae Choi, \email{Jae.Choi@@dfo-mpo.gc.ca}
 #' @references \url{data_blah.com}
